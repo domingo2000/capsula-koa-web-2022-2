d59c631ba7bc403fb9f2037b32bb5b5e2e9a8cfb
domingo2000 <domingo.edwards@uc.cl>
2022-08-16 22:30:30 -0400

    ft-added-get-route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import Koa from 'koa';
+import Router from '@koa/router';
+
 const app = new Koa();
+const router = new Router();
+
 
 const PORT = 3000;
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa!';
+router.get('/', (ctx, next) => {
+  ctx.body = "Hello world!"
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`);
