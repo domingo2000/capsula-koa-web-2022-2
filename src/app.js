f9bee2f75e82aac4bc579e374bad2bd055162171
domingo2000 <domingo.edwards@uc.cl>
2022-08-16 22:38:38 -0400

    ft-post-with-body-parsing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import Router from '@koa/router';
 import KoaLogger from 'koa-logger';
+import koaBody from 'koa-body';
 
 const app = new Koa();
 const router = new Router();
@@ -8,12 +9,21 @@ const router = new Router();
 // Log the requests of the server
 app.use(KoaLogger());
 
+// Parse the request body
+app.use(koaBody())
+
 
 const PORT = 3000;
 
 router.get('/', (ctx, next) => {
   ctx.body = "Hello world!"
 });
+
+
+router.post('root', '/', (ctx, next) => {
+  const message = ctx.request.body.message;
+  ctx.body = `Your message is: ${message}`;
+})
 
 app.use(router.routes()).use(router.allowedMethods());
 
