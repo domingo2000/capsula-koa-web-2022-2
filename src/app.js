531a89754a08ac29c369369aa74e2bd2a8163a7f
domingo2000 <domingo.edwards@uc.cl>
2022-08-16 22:32:36 -0400

    ft-added-logger-to-requests

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import Koa from 'koa';
 import Router from '@koa/router';
+import KoaLogger from 'koa-logger';
 
 const app = new Koa();
 const router = new Router();
+
+// Log the requests of the server
+app.use(KoaLogger());
 
 
 const PORT = 3000;
@@ -12,6 +16,7 @@ router.get('/', (ctx, next) => {
 });
 
 app.use(router.routes()).use(router.allowedMethods());
+
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`);
